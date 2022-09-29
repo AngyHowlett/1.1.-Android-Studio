7c270c8d8e55e180eaaa087aac2e9fdd1ed2129f
Svetlana <svetik0814@mail.ru>
2022-09-29 23:27:27 +0500

    Add new files, settings.gradle updated

--- app/src/main/java/ru/netology/a1_1_androidstudio/MainActivity.kt
+++ app/src/main/java/ru/netology/a1_1_androidstudio/MainActivity.kt
@@ -2,10 +2,54 @@ package ru.netology.a1_1_androidstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.a1_1_androidstudio.databinding.ActivityMainBinding
+import ru.netology.a1_1_androidstudio.dto.Post
+import ru.netology.a1_1_androidstudio.dto.counterView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 0L,
+            author = "Вася Пупкин",
+            content = "Привет! Это новый Адриано Челентано!",
+            published = "07.07.2022",
+        )
+
+        with(binding) {
+            authorName.text = post.author
+            textPost.text = post.content
+            date.text = post.published
+
+            likeText.text = counterView(post.counterLike)
+            repostText.text = counterView(post.conterRepost)
+            viewText.text = counterView(post.counterView)
+
+            if (post.likedByMe) {
+                likeButton.setImageResource(R.drawable.ic_red_favorite_24dp)
+            }
+
+            likeButton.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                binding.likeButton.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_red_favorite_24dp else R.drawable.ic_favorite_24dp
+                )
+
+                if (post.likedByMe) post.counterLike++ else post.counterLike--
+                likeText.text = counterView(post.counterLike)
+            }
+
+            repostButton.setOnClickListener {
+                post.conterRepost++
+                repostText.text = counterView(post.conterRepost)
+            }
+        }
     }
+
+
 }
---
+++ app/src/main/java/ru/netology/a1_1_androidstudio/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.a1_1_androidstudio.dto
+
+data class Post (
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var counterLike: Int = 0,
+    var conterRepost: Int = 0,
+    var counterView: Int = 0,
+    var likedByMe: Boolean = false
+)
